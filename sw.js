c097c051d58660d32dbd48041a8d037b9e1ad0a1
Kumar Ankit <ankit668@github.com>
2026-08-31 17:17:24 +0530

    feat: massively expand July 2026 current affairs with all minor facts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'ssc-prep-v81';
+﻿const CACHE_NAME = 'ssc-prep-v82';
 const urlsToCache = [
   './',
   './index.html',
@@ -89,5 +89,6 @@ self.addEventListener('activate', event => {
     })
   );
 });
+
 
 
